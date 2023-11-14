839a0e524aea613423f1b810d1d280cd88cb8205
alan-augusto <augustoalan56@gmail.com>
2023-11-14 08:50:26 -0300

    mudança de contexto

--- src/classes/RelatorioDesempenho.java
+++ src/classes/RelatorioDesempenho.java
@@ -3,11 +3,21 @@ import java.util.List;
 public class RelatorioDesempenho {
 
     public void gerarRelatorio(List<Questao> questoes, List<RespostaQuestao> respostas) {
-        // Lógica para gerar o relatório de desempenho com base nas questões e respostas fornecidas
+        // Verificar se as listas não estão vazias
+        if (questoes.isEmpty() || respostas.isEmpty()) {
+            System.out.println("Não há questões ou respostas para gerar o relatório.");
+            return;
+        }
 
-        // Aqui podemos calcular o tempo de execução, questões menos acertadas, etc.
+        // Verificar se as listas têm o mesmo tamanho
+        if (questoes.size() != respostas.size()) {
+            System.out
+                    .println("O número de questões é diferente do número de respostas. Impossível gerar o relatório.");
+            return;
+        }
 
-        // Exemplo básico:
+        // Lógica para gerar o relatório de desempenho com base nas questões e respostas
+        // fornecidas
         System.out.println("Relatório de Desempenho:");
         for (int i = 0; i < questoes.size(); i++) {
             Questao questao = questoes.get(i);

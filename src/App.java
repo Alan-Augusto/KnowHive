23fc08ab4fb382e601ff702cc25c238be97f5cd9
Davi Brito <davi.brito@embraer.com.br>
2023-11-16 10:14:22 -0300

    remover atributos nao utilizados

--- src/App.java
+++ src/App.java
@@ -6,10 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
-    public static List<Usuario> usuarios = new ArrayList<>();
-    public static Boolean login = false;
-
     public static void main(String[] args) {
         Simulacao simulacao = new Simulacao();
         simulacao.run();

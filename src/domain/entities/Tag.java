c9e40d22534367dacea7bbf6a4a70959367ecd39
Davi Brito <daviibrito111@gmail.com>
2023-11-18 19:00:28 -0300

    adicionar construtor com atributo

--- src/domain/entities/Tag.java
+++ src/domain/entities/Tag.java
@@ -3,6 +3,10 @@ package domain.entities;
 public class Tag {
     private String nome;
 
+    public Tag(String nome) {
+        this.nome = nome;
+    }
+
     public String getNome() {
         return nome;
     }

83f576b07670374253111007b1359a6a2f2739c8
Davi Brito <daviibrito111@gmail.com>
2023-11-19 02:20:55 -0300

    cria construtores

--- src/domain/entities/Alternativa.java
+++ src/domain/entities/Alternativa.java
@@ -4,6 +4,16 @@ public class Alternativa {
     private String texto;
     private boolean correta;
 
+    public Alternativa(String texto) {
+        this.texto = texto;
+        this.correta = false;
+    }
+
+    public Alternativa() {
+        this.texto = "";
+        this.correta = false;
+    }
+
     public String getTexto() {
         return texto;
     }
